bba4ee0c093f310a970e1c54d771c41529f336a3
sreshetnyak <reshetniak.sergey@gmail.com>
2014-03-27 12:32:34 +0200

    Initial commit from local git

---
+++ Calculator/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  Calculator
+//
+//  Created by sergey on 3/27/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  Calculator
+//
+//  Created by sergey on 3/27/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
